a3d5e6e2a5db8a801353c468cee55de661fb1357
Rahad-Ullah <myemail.abc20@gmail.com>
2024-01-17 23:45:43 +0600

    Configure Git and Github

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ function App() {
   return (
     <>
       <h1 className="text-3xl text-center">Sky Forecast</h1>
-      <h1 className="text-3xl text-center">Setup Git</h1>
+      <h1 className="text-3xl text-center">Setup Git and Github</h1>
     </>
   )
 }
